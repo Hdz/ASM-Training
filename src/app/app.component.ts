8de8cc44af6d542dd8f71a67ee3f495ba10715bd
Hdz <a.drouaud@gmail.com>
2022-06-07 18:54:45 +0200

    Ajout des boutons pour test template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { AuthService } from './shared/services/auth.service';
 import {MediaMatcher} from '@angular/cdk/layout';
-import {ChangeDetectorRef, Component, OnDestroy,OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +12,14 @@ import {ChangeDetectorRef, Component, OnDestroy,OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'La Plateforme de ASM';
   mobileQuery: MediaQueryList;
-
+  changeText: boolean;
   private _mobileQueryListener: () => void;
 
-  constructor(public authService: AuthService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(public authService: AuthService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addEventListener("change",this._mobileQueryListener);
+    this.changeText=false;
   }
   ngOnInit(): void { /* TODO document why this method 'ngOnInit' is empty */ }
 
